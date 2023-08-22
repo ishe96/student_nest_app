743d86dbd5a8f3fe389bc3bbae6c5647bc6a2fc2
ishe96 <amadamombe904@gmail.com>
2023-08-22 14:56:14 +0200

    Home page

---
+++ hotels.js
@@ -0,0 +1,36 @@
+const hotels = [
+    {
+      id: '1',
+      name: 'NUST Courts',
+      location: 'Bach street',
+      price: 1200,
+      image: require('./assets/hotel1.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    },
+    {
+      id: '2',
+      name: 'IUM Campus',
+      location: 'Dorado Park',
+      price: 7000,
+      image: require('./assets/hotel4.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    },
+    {
+      id: '3',
+      name: 'Wernhill Flats',
+      location: 'Almond street',
+      price: 9500,
+      image: require('./assets/hotel1.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    },
+    {
+      id: '4',
+      name: 'UNAM Campus',
+      location: 'Main street',
+      price: 10000,
+      image: require('./assets/hotel4.jpg'),
+      details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
+    },
+  ];
+  
+  export default hotels;
